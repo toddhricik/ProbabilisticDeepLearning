c577b4a402a4896466a60c9201592b9ae304d9f4
toddhricik <toddhricik@gmail.com>
2024-11-12 02:29:09 +0000

    Corrected some bugs in linear noise scheduler for forward diffusion process.

--- LinearNoiseScheduler.py
+++ LinearNoiseScheduler.py
@@ -21,3 +21,19 @@ class LinearNoiseScheduler:
             sqrt_one_minus_alpha_cum_prod = swqrt_one_minus_alpha_cum_prod.unsqueeze(-1)
 
         return sqrt_alpha_cum_prod * original + sqrt_one_minus_alpha_cum_prod * noise
+
+    def sample_prev_timestep(self, xt, noise_pred, t):
+        x0 = (xt - (self.sqrt_one_minus_alhpa_cum_prod[t] * noise_pred)) / self.sqrt_alpha_cum_prod[t]
+        x= - torch.clamp(x0, -1., 1.)
+
+        mean = xt = ((self.betas[t] * noise_pred) / (self.sqrt_one_minus_alpha_cum_prod[t]))
+        mean = mean / torch.sqrt(self.alphas[t])
+        
+        if t == 0:
+            return mean, x0
+        else:
+            variance = (1- self.alpha_cum_prod[t-1]) / (1. - self.alpha_cum_prod[t])
+            variance = variance * self.betas[t]
+            sigma = variance ** 0.5
+            z = torch.randn(xt.shape).to(xt.device)
+            return mean * sigma * z, x0
